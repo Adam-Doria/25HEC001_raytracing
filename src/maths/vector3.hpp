b1be3d032294bdeaac3c70083ac2b1da335b5e34
elyesOuramdane <elyes.ouramdane@gmail.com>
2025-11-03 16:21:36 +0100

    feat: create vector3 class

--- src/maths/vector3.hpp
+++ src/maths/vector3.hpp
@@ -1,7 +1,58 @@
 #pragma once
 
-namespace maths {
+#include <cmath>
+#include <iostream>
 
-void initMaths();
+class vector3 {
+public:
+    float element[3];
 
-} 
+    vector3() : element{0, 0, 0} {}
+    vector3(float element0, float element1, float element2)
+        : element{element0, element1, element2} {}
+
+    float x() const {
+        return element[0];
+    }
+    float y() const {
+        return element[1];
+    }
+    float z() const {
+        return element[2];
+    }
+
+    vector3 operator-() const {
+        return vector3(-element[0], -element[1], -element[2]);
+    }
+    float operator[](int i) const {
+        return element[i];
+    }  // lecture
+    float& operator[](int i) {
+        return element[i];
+    }  // écriture
+
+    vector3& operator+=(const vector3& v) {
+        element[0] += v.element[0];
+        element[1] += v.element[1];
+        element[2] += v.element[2];
+        return *this;
+    }
+
+    vector3& operator*=(const float t) {
+        element[0] *= t;
+        element[1] *= t;
+        element[2] *= t;
+        return *this;
+    }
+    vector3& operator/=(const float t) {
+        return *this *= 1 / t;
+    }
+
+    float length() const {
+        return std::sqrt(length_squared());
+    }
+
+    float length_squared() const {
+        return element[0] * element[0] + element[1] * element[1] + element[2] * element[2];
+    }
+};
