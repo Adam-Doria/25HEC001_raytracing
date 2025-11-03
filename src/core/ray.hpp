303d4991ef664a4183ce00738d3a48f26c8b7e34
Adam-Doria <drici.adam@gmail.com>
2025-11-03 21:21:09 +0100

    feat: add ray implementation

--- src/core/ray.hpp
+++ src/core/ray.hpp
@@ -1,7 +1,25 @@
 #pragma once
 
-namespace core {
+#include "maths/vector3.hpp"
 
-void initCore();
+class ray {
+public:
+    ray() {}
 
-} 
+    ray(const point3& origin, const vector3& direction) : orig(origin), dir(direction) {}
+
+    const point3& origin() const {
+        return orig;
+    }
+    const vector3& direction() const {
+        return dir;
+    }
+
+    point3 at(float t) const {
+        return orig + t * dir;
+    }
+
+private:
+    point3 orig;
+    vector3 dir;
+};
