1a128536b70a18ae4d61020d1bf612d08fa52b80
elyesOuramdane <elyes.ouramdane@gmail.com>
2025-11-04 17:04:00 +0100

    feat: enhance color calculation and add sphere intersection logic

--- src/core/color.hpp
+++ src/core/color.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <cmath>
+
 #include "maths/vector3.hpp"
 #include "ray.hpp"
 
@@ -21,13 +23,33 @@
  * - Noir profond en bas (quand le rayon pointe vers le bas)
  * - Bleu marine/indigo en haut (quand le rayon pointe vers le haut)
  */
+
+inline float hit_sphere(const point3& center, float radius, const ray& r) {
+    vector3 oc = center - r.origin();
+    auto a = dot(r.direction(), r.direction());
+    auto b = -2.0 * dot(r.direction(), oc);
+    auto c = dot(oc, oc) - radius * radius;
+    auto discriminant = b * b - 4 * a * c;
+
+    if (discriminant < 0) {
+        return -1.0f;  // Pas d'intersection
+    } else {
+        return (-b - std::sqrt(discriminant)) / (2.0 * a);
+    }
+}
+
 inline color ray_color(const ray& r) {
+    auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
+    if (t > 0.0) {
+        vector3 N = unit_vector(r.at(t) - vector3(0, 0, -1));
+        return 0.5f * color(N.x() + 1, N.y() + 1, N.z() + 1);
+    }
+
     vector3 unit_direction = unit_vector(r.direction());
 
-    auto t = 0.5f * (unit_direction.y() + 1.0f);
+    auto a = 0.5 * (unit_direction.y() + 1.0);
 
-    color deep_space = color(0.02f, 0.02f, 0.05f);
-    color space_blue = color(0.1f, 0.15f, 0.35f);
-
-    return (1.0f - t) * deep_space + t * space_blue;
+    color deep_space = color(0.02, 0.02, 0.05);
+    color space_blue = color(0.1, 0.15, 0.35);
+    return (1.0 - a) * deep_space + a * space_blue;
 }
